70eb4b3b45d421057c8c7300a7a3c12168e43087
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-07-27 16:02:28 +0200

    Add dht-monitor folder

---
+++ dht-monitor/dynamic_server.py
@@ -0,0 +1,58 @@
+import json
+import time
+from collections import defaultdict
+
+import requests
+from flask import Flask, request
+
+import send_data
+
+app = Flask(__name__)
+
+data_dict = defaultdict(int)
+# uuid_dict = defaultdict(int)
+last_update_time = time.time()
+
+
+@app.route("/topic", methods=["POST"])
+def receive_data():
+    global last_update_time, data_dict  # uuid_dict
+    data = request.get_json()
+    print("\nDATA:")
+    print(data)
+    name = data.get("topic_name", None)
+    # uuid = data.get('topic_uuid', None)
+
+    if name is not None:
+        data_dict[name] += 1
+    """
+    if uuid is not None:
+        uuid_dict[uuid] += 1
+    """
+    if time.time() - last_update_time >= 10:
+        with open("log_name.json", "a") as f:
+            try:
+                requests.post(
+                    "http://localhost:4545/dht_data", json=json.dumps(data_dict)
+                )
+            except:
+                pass
+            json.dump(dict(data_dict), f)
+            f.write("\n")
+            print("Sending DATA to SYSTEM PROTECTION MANAGER ...")
+            send_data.send_data(data_dict)
+        """
+        with open('log_uuid.json', 'a') as u:
+            json.dump(dict(uuid_dict), u)
+            u.write('\n')
+        """
+        data_dict = defaultdict(int)
+        # uuid_dict = defaultdict(int)
+        last_update_time = time.time()
+
+    # return response
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port="4747")
