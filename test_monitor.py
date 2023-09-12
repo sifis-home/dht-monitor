b5c029219bb581fce39235b2127f16d1e554e5c9
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-09-12 16:53:57 +0200

    Fix ruff error

--- test_monitor.py
+++ test_monitor.py
@@ -1,12 +1,16 @@
-from dht_monitor import catch_topic
 import json
-import requests
-import websocket
+from unittest.mock import patch
+
 import pytest
-from unittest.mock import patch, MagicMock
+import requests
+
+from dht_monitor import catch_topic
+
 
 def on_message(ws, message):
-    print("Received:")  # Assicurati che questa riga sia presente come prima istruzione
+    print(
+        "Received:"
+    )  # Assicurati che questa riga sia presente come prima istruzione
 
     json_message = json.loads(message)
 
@@ -28,20 +32,22 @@ def on_message(ws, message):
         else:
             print("Failed to send data to Flask server")
 
+
 # Funzione di test per on_error
 def test_on_error():
     # Mock delle chiamate a print
-    with patch('builtins.print') as mock_print:
+    with patch("builtins.print") as mock_print:
         error_message = "Test error message"
         catch_topic.on_error(None, error_message)
 
         # Verificare che la funzione di stampa sia stata chiamata con il messaggio di errore corretto
         mock_print.assert_called_once_with(error_message)
 
+
 # Funzione di test per on_close
 def test_on_close():
     # Mock delle chiamate a print
-    with patch('builtins.print') as mock_print:
+    with patch("builtins.print") as mock_print:
         close_status_code = 1000
         close_msg = "Connection closed"
         catch_topic.on_close(None, close_status_code, close_msg)
@@ -49,15 +55,17 @@ def test_on_close():
         # Verificare che la funzione di stampa sia stata chiamata con i parametri di chiusura corretti
         mock_print.assert_called_once_with("### Connection closed ###")
 
+
 # Funzione di test per on_open
 def test_on_open():
     # Mock delle chiamate a print
-    with patch('builtins.print') as mock_print:
+    with patch("builtins.print") as mock_print:
         catch_topic.on_open(None)
 
         # Verificare che la funzione di stampa sia stata chiamata con il messaggio di connessione corretto
         mock_print.assert_called_once_with("### Connection established ###")
 
+
 # Eseguire i test
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main()
